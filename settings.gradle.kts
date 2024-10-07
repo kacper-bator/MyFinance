071faced5ff5ddf7457d0145b1d6d27508f34ddd
kacper-bator <62474644+kacper-bator@users.noreply.github.com>
2024-10-07 16:33:02 +0200

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,22 @@
+import java.net.URI
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+@Suppress("UnstableApiUsage")
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { setUrl("https://jitpack.io") }
+    }
+}
+
+rootProject.name = "MyFinance"
+include(":app")
+ 
